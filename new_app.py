ffb560d16ceb97539df0196614ae9efc1e8a36ee
Hemant8503 <118989974+Hemant8503@users.noreply.github.com>
2024-01-05 15:07:30 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -5,7 +5,7 @@ import pickle
 # Load the trained model
 
 model = pickle.load(open("C:\\Users\\hp\\Desktop\\car_price\\updated_model_CP.pkl" , 'rb'))
-df = pd.read_csv("C:\\Users\\hp\\Desktop\\car_price\\car data.csv")
+df = pd.read_csv("car data.csv")
 
 
 # Define the user input interface
